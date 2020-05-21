92796015648a3e51dc1873298ad64cb0064bdfd2
Coding Unicorn <co.unicorn.ding@gmail.com>
2020-05-21 15:01:54 +0700

    tyding

--- app.js
+++ app.js
@@ -23,13 +23,13 @@ function mono(text) {
 
   function char(c) {
     const charOffset = 0x1d670;
-    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
     return chars.includes(c) ? String.fromCodePoint(chars.indexOf(c) + charOffset) : c;
   }
 
   function num(c) {
     const numOffset = 0x1d7f6;
-    const numbers = '0123456789'
+    const numbers = '0123456789';
     return numbers.includes(c) ? String.fromCodePoint(numbers.indexOf(c) + numOffset) : c;
   }
 
